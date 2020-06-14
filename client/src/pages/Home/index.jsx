5548460487debe0a0010a38208f8d687a2d7a9ea
Nate Foster <soundwubz@gmail.com>
2020-06-14 12:35:52 -0400

    style update; link-text global css class

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -26,7 +26,7 @@ class Home extends React.Component {
                     <div className="col">
                         <p className="text-right gh-text">
                             Take a look at my Github pages to view my live projects.
-                            You can also view the project page for direct links to live websites.
+                            You can also view the <a href="/projects" className="text-link">project page</a> for direct links to live websites.
                         </p>
                     </div>
                     <div className="col-1">
@@ -42,8 +42,8 @@ class Home extends React.Component {
                             <svg className="bi bi-phone" style={
                                 { marginTop: "14px", marginBottom: "18px" }
                             } width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M11 1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
-                                <path fill-rule="evenodd" d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                                <path fillRule="evenodd" d="M11 1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+                                <path fillRule="evenodd" d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
                             </svg>
                             <p style={{ display: "inline-block", margin: "7px 0px", paddingLeft: "10px"}}>Contact</p>
                         </button>
